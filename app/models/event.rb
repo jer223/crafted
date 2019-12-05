99a8ab61bcaac7946cbc72de7c6d0f8b3460d48f
Jeremy Hall <jhall229@gmail.com>
2019-12-05 16:53:09 +0000

    seting up geocoding for map feature

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
 #  validates :local, presence :true
 #  validates :date, presence :true
+  geocoded_by :address
+  after_validation :geocode
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_170434) do
+ActiveRecord::Schema.define(version: 2019_12_05_160316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_08_10_170434) do
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["date"], name: "index_events_on_date"
   end
 
